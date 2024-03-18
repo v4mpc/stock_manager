065d1bb5bc6696fdc5108507edfaaad0fe1b74e6
Yona Mahundi <yona101992@gmail.com>
2024-03-18 15:16:32 +0300

    minor fixes

--- core/util.py
+++ core/util.py
@@ -21,18 +21,20 @@ class UpdateStockOnInactiveProductException(Exception):
 def update_stock_on_hand(product: Product, stock_date: date, quantity: float, transaction_type: str):
     if not product.active:
         raise UpdateStockOnInactiveProductException
-    try:
-        obj = StockOnHand.objects.get(product=product,
-                                      created_at=stock_date)
-        if transaction_type == 'DR':
-            obj.quantity += quantity
-        else:
-            obj.quantity -= quantity
-        obj.save()
-    except StockOnHand.DoesNotExist:
+
+    obj = StockOnHand.objects.filter(product=product)
+    if obj.count() == 0:
         new_values = {"product": product, "created_at": stock_date, "quantity": quantity}
         obj = StockOnHand(**new_values)
         obj.save()
+        return
+    latest_result = obj.latest('created_at')
+    latest_result.created_at = stock_date
+    if transaction_type == 'DR':
+        latest_result.quantity += quantity
+    else:
+        latest_result.quantity -= quantity
+    latest_result.save()
 
 
 def extract_quantity(queryset):
