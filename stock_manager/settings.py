7448e4863c6270868b6fcee9936fa850539d32a2
Yona Mahundi <yona101992@gmail.com>
2024-03-20 13:33:08 +0300

    added allowed hosts

--- stock_manager/settings.py
+++ stock_manager/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-u4latv7#rs(wuk_qltdy2mj2amuu*=q$pw+a_8!py05h3w9d_3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['178.79.152.43']
 
 # Application definition
 
