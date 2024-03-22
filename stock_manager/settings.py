3d88dcb9291e2f1ec05f31236ed0e5bcee4bda0e
Yona Mahundi <yona101992@gmail.com>
2024-03-22 14:33:33 +0300

    changed database engine

--- stock_manager/settings.py
+++ stock_manager/settings.py
@@ -78,24 +78,24 @@ WSGI_APPLICATION = 'stock_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-#
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "smt",
-#         "USER": "smt_user",
-#         "PASSWORD": "smt_password_to_be_used",
-#         "HOST": "db",
-#         "PORT": "5432",
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "smt",
+        "USER": "smt_user",
+        "PASSWORD": "smt_password_to_be_used",
+        "HOST": "db",
+        "PORT": "5432",
+    }
+}
 
 AUTH_USER_MODEL = "core.User"
 
